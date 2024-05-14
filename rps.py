070a1c95d4e07005abe1021f8b5b60ab9071981b
chriswebdv <chriswebdv@gmail.com>
2024-05-13 18:35:29 -0600

    Create function which request input from user

---
+++ rps.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_choices():
+    player_choice = input("Enter a choice: rock, paper, scissors ")
+    computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+get_choices()
+
+
+
