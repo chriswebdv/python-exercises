cb769654cf8bd9e7adbc2a3a4e3e8bf343128912
chriswebdv <chriswebdv@gmail.com>
2024-05-19 08:40:49 -0600

    Create str method to print the Coordite object

--- MIT-PY/lesson-oop-ex1.py
+++ MIT-PY/lesson-oop-ex1.py
@@ -8,8 +8,12 @@ class Coordinate(object):
         y_diff_sq = (self.y-other.y)**0.5
         return (x_diff_sq + y_diff_sq)**0.5
 
+    def __str__(self):
+        return "<"+str(self.x)+","+str(self.y)+">"
 
 c = Coordinate(3,4)
 origin = Coordinate(0,0)
 print(c.x)
 print(origin.x)
+print(c.distance(origin))
+print(c)
